4ce632bb339387bbb7c6f75651aa9e32a1924f47
nishi <nishi.naoki1652@gmail.com>
2025-07-27 19:41:28 +0900

    fix: add debug output to main.py

--- main.py
+++ main.py
@@ -6,21 +6,21 @@
 # 1. ライブラリの読み込み
 ############################################################
 # 「.env」ファイルから環境変数を読み込むための関数
-from dotenv import load_dotenv
-# ログ出力を行うためのモジュール
-import logging
+# from dotenv import load_dotenv
 # streamlitアプリの表示を担当するモジュール
 import streamlit as st
+# （自作）変数（定数）がまとめて定義・管理されているモジュール
+import constants as ct
+# ↓必ず最初に書く
+st.set_page_config(page_title=ct.APP_NAME)  
+# （自作）画面表示系の関数が定義されているモジュール
+import components as cn
+# ログ出力を行うためのモジュール
+import logging
 # （自作）画面表示以外の様々な関数が定義されているモジュール
 import utils
 # （自作）アプリ起動時に実行される初期化処理が記述された関数
 from initialize import initialize
-# （自作）画面表示系の関数が定義されているモジュール
-import components as cn
-# （自作）変数（定数）がまとめて定義・管理されているモジュール
-import constants as ct
-
-st.set_page_config(page_title=ct.APP_NAME)  # ← 必ず最初に書く
 
 ############################################################
 # 2. 設定関連
